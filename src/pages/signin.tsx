aaccae7a88adc3695e6726fcc58363a95aa0ef9c
Rommel De Torres <rommel.detorres@triseed.com.ph>
2022-06-08 11:20:45 +0800

    Added signin api integration

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,14 +1,46 @@
+import { yupResolver } from '@hookform/resolvers/yup'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useCallback } from 'react'
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
 
+import { useSignin } from '@/api'
 import Key from '@/assets/images/key.png'
 import DividerText from '@/components/DividerText'
 import FormCheckbox from '@/components/FormCheckbox'
 import FormInput from '@/components/FormInput'
 import PublicLayout from '@/components/layout/PublicLayout'
+import Spinner from '@/components/Spinner'
+
+const schema = yup.object({
+  email: yup.string().email().required('Required field'),
+  password: yup
+    .string()
+    .min(6, 'Password must be at least 6 characters')
+    .required('Required field')
+})
 
 const SignIn = () => {
+  const { execute, isLoading } = useSignin()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({ resolver: yupResolver(schema) })
+
+  const onSubmit = useCallback(
+    async (data: any) => {
+      console.log(data)
+      try {
+        await execute(data)
+      } catch (e) {
+        console.error(e)
+      }
+    },
+    [execute]
+  )
   return (
     <PublicLayout>
       <div className='flex items-center gap-16 py-16 pr-32'>
@@ -34,17 +66,51 @@ const SignIn = () => {
               <p className='text-black25'>or</p>
             </DividerText>
           </div>
-          <form className='flex w-2/6 flex-col gap-4'>
-            <FormInput label='Email Address' />
-            <FormInput label='Password' type='password' />
-            <FormCheckbox>
+          <form
+            className='flex w-2/6 flex-col gap-4'
+            onSubmit={handleSubmit(onSubmit)}>
+            <FormInput
+              register={register}
+              fieldName='email'
+              label='Email Address'
+              placeholder='Enter your email address...'
+            />
+            {Boolean(errors.email) && (
+              <span className='text-sm text-red-400'>
+                {errors.email?.message}
+              </span>
+            )}
+            <FormInput
+              register={register}
+              fieldName='password'
+              label='Password'
+              placeholder='Enter your password...'
+              type='password'
+            />
+            {Boolean(errors.password) && (
+              <span className='text-sm text-red-400'>
+                {errors.password?.message}
+              </span>
+            )}
+            <FormCheckbox register={register} fieldName='terms'>
               <span className='text-sm text-black25'>
                 Remember me (Please use this feature on a private device)
               </span>
             </FormCheckbox>
+            {Boolean(errors.terms) && (
+              <span className='text-sm text-red-400'>
+                {errors.terms?.message}
+              </span>
+            )}
             <div className='mt-8 flex flex-col items-center justify-center gap-2'>
-              <button className='rounded-full bg-gradient-to-r from-secondary to-primary py-2 px-6'>
-                <h5 className='text-xl font-bold text-white'>Sign In</h5>
+              <button
+                className={`rounded-full bg-gradient-to-r from-secondary to-primary py-2 px-6 ${
+                  isLoading ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'
+                }`}
+                disabled={isLoading}>
+                <h5 className='flex gap-2 text-xl font-bold text-white'>
+                  {isLoading && <Spinner />}Sign in
+                </h5>
               </button>
               <p className='text-sm text-black25'>
                 Forgot password? Click{' '}
