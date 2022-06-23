df384d4ae9148c7815f767e6527a81532e4fabf4
Rommel De Torres <rommel.detorres@triseed.com.ph>
2022-06-23 15:23:10 +0800

    added campaign page

---
+++ src/pages/app/campaign.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+import AppNavBar from '@/components/AppNavBar'
+import PrivateLayout from '@/components/layout/PrivateLayout'
+import CampaignImage from '@/assets/images/campaign.png'
+import CampaignPostCard from '@/assets/images/campaign-card.png'
+import CampaignEnvelope from '@/assets/images/envelope-card.png'
+import CampaignCard from '@/containers/Campaign/campaigncard'
+import CampaignHeader from '@/containers/Campaign/CampaignHeader'
+
+const Campaign = () => {
+  return (
+    <PrivateLayout>
+      <AppNavBar pageName='Campaign' />
+      <CampaignHeader image={CampaignImage}></CampaignHeader>
+      <div className='gap-18 mt-12 grid grid-cols-2'>
+        <CampaignCard
+          image={CampaignPostCard}
+          title={'4 x 6 POSTCARD'}
+          description={'(4 inches to 6 inches)'}
+          detailOne={'• 14 pt Gloss Paper Stock'}
+          detailTwo={'• Full Color Double Sided'}></CampaignCard>
+        <CampaignCard
+          image={CampaignPostCard}
+          title={'6 x 9 POSTCARD'}
+          description={'(6 inches to 9 inches)'}
+          detailOne={'• 70# Paper Stock'}
+          detailTwo={'• #10 Window Envelope'}></CampaignCard>
+      </div>
+      <div className='gap-18 mt-12 grid grid-cols-2'>
+        <CampaignCard
+          image={CampaignPostCard}
+          title={'6 x 11 POSTCARD'}
+          description={'(6 inches to 11 inches)'}
+          detailOne={'• 14pt Gloss Paper Stock'}
+          detailTwo={'• Full Color Double Sided'}></CampaignCard>
+        <CampaignCard
+          image={CampaignEnvelope}
+          title={'8.5 x 11 LETTER'}
+          description={'(8.5 inches to 11 inches)'}
+          detailOne={'• 70# Paper Stock'}
+          detailTwo={
+            '• Large Window Envelope to Show Logo and Sender Address'
+          }></CampaignCard>
+      </div>
+    </PrivateLayout>
+  )
+}
+
+export default Campaign
