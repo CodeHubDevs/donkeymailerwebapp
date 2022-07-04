c0f20f26df532498f6e4a3043d8a4334b6ee915d
Rommel De Torres <rommel.detorres@triseed.com.ph>
2022-07-04 20:32:36 +0800

    Added guide footer for dashboard

---
+++ src/components/GuideInfoFooter.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { faPlay } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import PdfFileIcon from '@/assets/images/pdf-icon.png'
+import QuestionMarkIcon from '@/assets/images/question-icon.png'
+import Image from 'next/image'
+
+interface GuideInfoFooterProps {
+  src: string
+}
+
+const GuideInfoFooter: React.FC<GuideInfoFooterProps> = ({ src }) => {
+  return (
+    <div className='mt-5 '>
+      <h2 className='mb-2 text-2xl font-bold'>Guides</h2>
+      <div className='flex h-10 items-center justify-center pl-16 pr-16'>
+        <div className='h-px w-full bg-black10' />
+      </div>
+      <div className='ml-12 grid grid-flow-col grid-rows-2 items-center justify-center justify-around'>
+        <div className='row-span-3 mx-auto bg-white p-2'>
+          <iframe
+            src={src}
+            className='aspect-video w-full'
+            height={300}
+            width={600}></iframe>
+        </div>
+        <div className='col-span-2'>
+          <button className='hover:bg-grey text-grey-darkest inline-flex h-20 w-64 items-center rounded-xl bg-white py-2 px-4 font-bold'>
+            <Image src={PdfFileIcon} alt='logo' />
+            <span className='ml-8 mr-16 text-lg'>Guides</span>
+            <FontAwesomeIcon icon={faPlay} fontSize={15} />
+          </button>
+        </div>
+        <div className='col-span-2'>
+          <button className='hover:bg-grey text-grey-darkest inline-flex h-20 w-64 items-center rounded-xl bg-white py-2 px-4 font-bold'>
+            <Image src={QuestionMarkIcon} />
+            <span className='ml-8 mr-16 text-lg'>FAQ</span>
+            <FontAwesomeIcon icon={faPlay} fontSize={15} />
+          </button>
+        </div>
+      </div>
+      <div className='flex h-10 items-center justify-center pl-16 pr-16'>
+        <div className='h-px w-full bg-black10' />
+      </div>
+    </div>
+  )
+}
+
+export default GuideInfoFooter
--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -5,6 +5,7 @@ import PrivateLayout from '@/components/layout/PrivateLayout'
 import CampaignTable from '@/containers/Dashboard/CampaignTable'
 import DashboardCards from '@/containers/Dashboard/DashboardCards'
 import DashboardCharts from '@/containers/Dashboard/DashboardCharts'
+import GuideInfoFooter from '@/components/GuideInfoFooter'
 
 const Dashboard = () => {
   return (
@@ -13,6 +14,7 @@ const Dashboard = () => {
       <DashboardCards />
       <CampaignTable />
       <DashboardCharts />
+      <GuideInfoFooter src='https://player.vimeo.com/video/146022717?color=0c88dd&title=0&byline=0&portrait=0&badge=0' />
     </PrivateLayout>
   )
 }
