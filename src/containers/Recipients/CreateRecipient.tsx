b308a44ad3b89a3599e2290b71c6ca785e683dd3
ervnjmsdnts <earvinjamesdantes@gmail.com>
2022-07-21 01:52:00 +0800

    Added recipients table

--- src/containers/Recipients/CreateRecipient.tsx
+++ src/containers/Recipients/CreateRecipient.tsx
@@ -1,3 +1,5 @@
+import { faCirclePlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
 import GoBackToSelectButton from '@/components/GoBackToSelectButton'
@@ -12,7 +14,7 @@ const CreateRecipient = () => {
       <div className='mt-4 rounded-xl bg-white p-4 shadow-lg'>
         <form className='flex flex-col items-start gap-2'>
           <div className='flex items-center gap-2'>
-            <h4>Name of Group</h4>
+            <h4 className='text-xl font-bold'>Name of Group</h4>
             <p>(First, create your recipient group name)</p>
           </div>
           <input
@@ -24,6 +26,77 @@ const CreateRecipient = () => {
             Save
           </button>
         </form>
+        <div>
+          <div className='mt-4 flex items-center justify-between border-t py-2'>
+            <h3 className='text-xl font-bold'>Recipients</h3>
+            <div className=''>
+              <button
+                type='button'
+                className='rounded-lg bg-primary from-secondary to-primary py-2 px-8 text-white '>
+                <span className='absolute inset-y-0 left-0 flex items-center pl-3'></span>
+                <FontAwesomeIcon
+                  icon={faCirclePlus}
+                  className='mt-1 cursor-pointer rounded-full bg-primary text-white'
+                />
+                <span className='ml-2 text-center text-sm font-bold text-white'>
+                  Add Recipient
+                </span>
+              </button>
+            </div>
+          </div>
+          <table className='w-full text-left text-sm text-gray-500'>
+            <thead className=' className border-b border-t text-sm text-gray-700'>
+              <tr>
+                <th scope='col' className='p-4'>
+                  <div className='flex items-center'>
+                    <input
+                      id='checkbox-all'
+                      type='checkbox'
+                      className='mr-2 border border-primary text-primary outline-primary checked:bg-primary hover:bg-primary focus:ring-primary'
+                    />
+                    All
+                  </div>
+                </th>
+                <th scope='col' className='px-6 py-3'>
+                  First Name
+                </th>
+                <th scope='col' className='px-6 py-3'>
+                  Last Name
+                </th>
+                <th scope='col' className='px-6 py-3'>
+                  Company Name
+                </th>
+                <th scope='col' className='px-6 py-3'>
+                  Address 1
+                </th>
+                <th scope='col' className='px-6 py-3'>
+                  Address 2
+                </th>
+                <th scope='col' className='px-6 py-3'>
+                  City
+                </th>
+                <th scope='col' className='px-6 py-3'>
+                  County/State
+                </th>
+                <th scope='col' className='px-6 py-3'>
+                  Postal/Zip Code
+                </th>
+                <th scope='col' className='px-6 py-3'>
+                  Country
+                </th>
+                <th scope='col' className='px-6 py-3'>
+                  Phone Number
+                </th>
+                <th scope='col' className='px-6 py-3'>
+                  Joined Date
+                </th>
+                <th scope='col' className='px-6 py-3'>
+                  <span className='className-only'></span>
+                </th>
+              </tr>
+            </thead>
+          </table>
+        </div>
       </div>
     </div>
   )
