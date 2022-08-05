b09fc74f2bb74c5c3e2d290dfa16c60c7f8362a2
Rommel De Torres <rommel.detorres@triseed.com.ph>
2022-08-05 23:59:24 +0800

    integration fetch templates board

--- src/containers/Templates/TemplateCard.tsx
+++ src/containers/Templates/TemplateCard.tsx
@@ -10,12 +10,12 @@ interface TemplateCardProps {
 const TemplateCard: React.FC<TemplateCardProps> = ({ name, type, image }) => {
   return (
     <div className='flex flex-col items-center justify-center gap-4 rounded-lg bg-black5 p-8 shadow-lg'>
-      <div className='relative h-48 w-80'>
-        <Image src={image} alt='Image' layout='fill' objectFit='cover' />
+      <div className='relative h-48 w-80 p-6'>
+        <Image src={image} alt='Image' objectFit='cover' />
       </div>
       <div className='text-center'>
         <h3 className='text-lg font-bold'>{name}</h3>
-        <p>{type}</p>
+        <p>{type} Postcard</p>
       </div>
       <button className='rounded-full bg-primary py-1 px-4 font-bold text-white hover:bg-secondary'>
         View Template
